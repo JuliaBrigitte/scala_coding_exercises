986515b8dbc6f83785d807f563462b903a5cd0cc
Julia Branke <Julia.Branke@gmx.co.uk>
2022-03-09 21:41:07 +0000

    Implemented class coordinates

--- src/main/scala/com/techreturners/marsRover/Coordinates.scala
+++ src/main/scala/com/techreturners/marsRover/Coordinates.scala
@@ -5,9 +5,49 @@ class Coordinates(xVar: Int, yVar: Int)
   var x:Int =xVar;
   var y:Int =yVar;
 
+  val maxGrid=5
+
   def move(dx: Int, dy: Int): Unit =
   {
     x=x+dx;
     y=y+dy;
+  }
+
+  def currentStatus(): String =
+  {
+    var result=x.toString + "," + y.toString
+    result
+  }
+
+  def moveNorth(): Unit =
+  {
+    if (maxGrid > y)
+    {
+      move(0,1)
+    }
+  }
+
+  def moveEast(): Unit =
+  {
+    if (0 < x)
+    {
+      move(-1,0)
+    }
+  }
+
+  def moveSouth(): Unit =
+  {
+    if (0 < y)
+    {
+      move(0,-1)
+    }
+  }
+
+  def moveWest(): Unit =
+  {
+    if (maxGrid > x)
+    {
+      move(1,0)
+    }
   }
 }
--- src/test/scala/com/techreturners/marsRover/marsRoverTest.scala
+++ src/test/scala/com/techreturners/marsRover/marsRoverTest.scala
@@ -27,6 +27,46 @@ class marsRoverTest extends AnyFlatSpec
     assert(direction.currentStatus() == 'N');
   }
 
+  var coordinates=new Coordinates(1,1);
+
+  "Move along the grid" should "move by one and stop at edges" in
+  {
+    coordinates.moveEast()
+    assert(coordinates.currentStatus() == "0,1");
+    coordinates.moveEast()
+    assert(coordinates.currentStatus() == "0,1");
+    coordinates.moveSouth()
+    assert(coordinates.currentStatus() == "0,0");
+    coordinates.moveSouth()
+    assert(coordinates.currentStatus() == "0,0");
+    coordinates.moveWest()
+    assert(coordinates.currentStatus() == "1,0");
+    coordinates.moveWest()
+    assert(coordinates.currentStatus() == "2,0");
+    coordinates.moveWest()
+    assert(coordinates.currentStatus() == "3,0");
+    coordinates.moveWest()
+    assert(coordinates.currentStatus() == "4,0");
+    coordinates.moveWest()
+    assert(coordinates.currentStatus() == "5,0");
+    coordinates.moveWest()
+    assert(coordinates.currentStatus() == "5,0");
+    coordinates.moveEast()
+    assert(coordinates.currentStatus() == "4,0");
+    coordinates.moveNorth()
+    assert(coordinates.currentStatus() == "4,1");
+    coordinates.moveNorth()
+    assert(coordinates.currentStatus() == "4,2");
+    coordinates.moveNorth()
+    assert(coordinates.currentStatus() == "4,3");
+    coordinates.moveNorth()
+    assert(coordinates.currentStatus() == "4,4");
+    coordinates.moveNorth()
+    assert(coordinates.currentStatus() == "4,5");
+    coordinates.moveNorth()
+    assert(coordinates.currentStatus() == "4,5");
+
+  }
 //  marsRover=new MarsRover([2,3,"N"]);
 //
 //"Mars Rover facing north" should "turn left to W then S then E in
